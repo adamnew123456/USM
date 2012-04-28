f038c30e38c94b5352bbe525ce7b169ba07b223c
Adam Marchetti <adamnew123456@gmail.com>
2012-04-28 14:44:18 +0000

    Fixed some bootstrap typos.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Takes the USM that is distributed in the Git repo and
 # copies it to the user's ~/Apps.
-python bin/usm.py init
+python bin/usm init
 cp -R bin ~/Apps/install
-cp -R share ~/Apps/install
-python bin/usm.py add usm 1.0
+cp -R man ~/Apps/install
+python bin/usm add usm 1.0
