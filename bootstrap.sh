b50999e6462e83f4a78d08f064648a944ecc29fe
Adam Marchetti <adamnew123456@gmail.com>
2012-05-18 08:19:37 -0300

    Continuing to fix Arch compatability.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Takes the USM that is distributed in the Git repo and
 # copies it to the user's ~/Apps.
-python bin/usm init
+python2 bin/usm init
 cp -R bin ~/Apps/install
 cp -R man ~/Apps/install
-python bin/usm add usm 1.0
+python2 bin/usm add usm 1.0
