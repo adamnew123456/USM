0482387c1256b56ec6911044f14e21e3bfc7a7cc
Adam Marchetti <adamnew123456@adam.(none)>
2013-03-14 21:03:37 -0400

    Fixed a bug in the bootstrap script.
      - The bootstrapper would attempt to delete a nonexistant directory
         ("$OME/Apps/usm--$VERSION") even though usm del successfully
         removed it anyway.

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,7 +7,6 @@ if [ -e "$HOME/Apps" ]; then
     # The user is updating in this case. Just copy everything over.
     echo "You have an older version of USM installed - answer Y at the next prompt to remove it."
     python2 bin/usm del usm
-    rm -f "$OME/Apps/usm--$VERSION"
     cp -R bin ~/Apps/install
     cp -R man ~/Apps/install
     python2 bin/usm add usm $VERSION
