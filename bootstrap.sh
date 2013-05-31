ad2a51d804782cb0c0325061f2ec65d77f0ce9a1
adamnew123456 <adamnew123456@gmail.com>
2013-05-31 11:14:29 -0400

    Conversion to Python 3.
    Entirely handled by the 2to3 tool.

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,14 +6,14 @@ VERSION=1.12
 if [ -e "$HOME/Apps" ]; then
     # The user is updating in this case. Just copy everything over.
     echo "You have an older version of USM installed - answer Y at the next prompt to remove it."
-    python2 bin/usm del usm
+    python3 bin/usm del usm
     cp -R bin ~/Apps/install
     cp -R man ~/Apps/install
-    python2 bin/usm add usm $VERSION
+    python3 bin/usm add usm $VERSION
 else
     # Fresh installation - no problems.
-    python2 bin/usm init
+    python3 bin/usm init
     cp -R bin ~/Apps/install
     cp -R man ~/Apps/install
-    python2 bin/usm add usm $VERSION
+    python3 bin/usm add usm $VERSION
 fi
