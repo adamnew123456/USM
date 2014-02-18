0d98ba1df7817f84f3d863a5db577b0603aaeb3a
adamnew123456 <adamnew123456@gmail.com>
2014-02-17 19:17:54 -0500

    Added and documented two new features:
     - The `usm copy-script` feature makes migration easier since it is not
       necessary to copy the script from bin/usm into ~/.usm-env
     - The `usm ls` command now takes an optional software argument which will
       list the versions of one particular software.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 # Takes the USM that is distributed in the Git repo and
 # copies it to the user's ~/Apps.
-VERSION=1.15
+VERSION=1.16
 
 if [ -e "$HOME/Apps" ]; then
-    # The user is updating in this case. Just copy everything over.
+    # The user is updating in this case. Copy everthing over, including the
+    # (possibly changed) startup script and then use the new version by default.
     cp -R bin ~/Apps/install
     cp -R man ~/Apps/install
     python3 bin/usm add usm $VERSION
+    python3 bin/usm copy-script
+    python3 bin/usm link usm $VERSION
 else
     # Fresh installation - no problems.
     python3 bin/usm init
