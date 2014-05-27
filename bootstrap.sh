8b674e27815095eda5623ea8d31b518cf40e4a54
adamnew123456 <adamnew123456@gmail.com>
2014-05-27 09:28:27 -0400

    Rewrote the USM scripts and man-pages.
    This rewrite brings along several changes:
     - Ability to configure where USM stores its programs.
     - Somewhat higher code quality (I hope).
     - Better documentation.

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,56 @@
 #!/bin/sh
 # Takes the USM that is distributed in the Git repo and
 # copies it to the user's ~/Apps.
-VERSION=1.18
+VERSION=2.0
+USM="bin/usm"
 
-if [ -e "$HOME/Apps" ]; then
+if [ -n "$1" ]; then
+    export USM_PATH="$1"
+elif [ -n "$USM_PATH" ]; then
+    # No use copying an already-existing environment variable
+    :
+else
+    export USM_PATH="$HOME/Apps"
+fi
+
+if [ -e "$USM_PATH/usm" ]; then
     # The user is updating in this case. Copy everthing over, including the
     # (possibly changed) startup script and then use the new version by default.
-    cp -R bin ~/Apps/install
-    cp -R man ~/Apps/install
-    python3 bin/usm add usm $VERSION
-    python3 bin/usm copy-script
-    python3 bin/usm link usm $VERSION
+    python3 $USM add usm $VERSION
+    USM_INSTALL_PATH="$(python3 $USM path usm $VERSION)"
+    cp -R bin "$USM_INSTALL_PATH"
+    mkdir "$USM_INSTALL_PATH/man"
+    cp -R man1 "$USM_INSTALL_PATH/man"
+    python3 $USM set-current usm $VERSION
+
+    chmod +x "$USM_INSTALL_PATH/bin/usm"
+    chmod +x "$USM_INSTALL_PATH/bin/usm-lib-helper"
+
+    python3 $USM copy-script
+    echo "**********"
+    echo 'You may want to `source ~/.usm-env` in your current shell'
+elif [ -e "$USM_PATH" ]; then
+    # If we can't find our previous installation, then assume that the user is 
+    # using $USM_PATH for something other than USM.
+    echo "If you would like to use another directory, please pass it as an option"
+    echo "to this script. Otherwise, pleace rename or remove $USM_PATH to allow"
+    echo "USM to use $USM_PATH for storing its applications. Aborting."
+    exit 1
 else
-    # Fresh installation - no problems.
-    python3 bin/usm init
-    cp -R bin ~/Apps/install
-    cp -R man ~/Apps/install
-    python3 bin/usm add usm $VERSION
+    # Fresh installation, without any issues
+    echo "Installing USM to $USM_PATH..."
+    python3 $USM init
+    python3 $USM add usm $VERSION
+    USM_INSTALL_PATH="$(python3 $USM path usm $VERSION)"
+    cp -R bin "$USM_INSTALL_PATH"
+    mkdir "$USM_INSTALL_PATH/man"
+    cp -R man1 "$USM_INSTALL_PATH/man"
+
+    chmod +x "$USM_INSTALL_PATH/bin/usm"
+    chmod +x "$USM_INSTALL_PATH/bin/usm-lib-helper"
+
+    echo "**********"
+    echo 'The USM startup script is now located in $HOME/.usm-env - you should add '
+    echo '`source ~/.usm-env` to the end of your shell RC file. If you wish to'
+    echo 'begin using USM in this shell, you may source it now.'
 fi
